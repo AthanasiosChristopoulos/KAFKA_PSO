9cef7a213b93f139c559caeafe3e22de90a585f1
AthanasiosChristopoulos <athanasioschristopoulos61@gmail.com>
2026-03-17 09:21:49 +0000

    gdf

--- java/src/main/java/experimentation/Experimentation.java
+++ java/src/main/java/experimentation/Experimentation.java
@@ -73,7 +73,7 @@ public class Experimentation {
         // =====================================================
 
         if(cfg.EXPERIMENTATION_MODE.equals("N_WORKERS")) {
-            cfg.EARLY_STOPPING = false;
+            cfg.EARLY_STOPPING = true;
             // Path csvPath = createUniqueCsvPath(cfg.EXPERIMENTATION_DIR, "results");
             Path dir = Path.of(cfg.EXPERIMENTATION_DIR);
             Files.createDirectories(dir);
@@ -83,7 +83,9 @@ public class Experimentation {
             // List<Integer> workersList = List.of(2, 12, 24); 
             // List<Integer> workersList = List.of(1, 2, 6, 12); // ignore 1 (warm up) just see 2, 12, 24
             // List<Integer> workersList = List.of(1, 2, 6, 12, 16); // ignore 1 (warm up) just see 2, 12, 24
-            List<Integer> workersList = List.of(1, 2, 6, 12, 16, 20); 
+            // List<Integer> workersList = List.of(1, 2, 6, 12, 16, 20); 
+            List<Integer> workersList = List.of(1, 2, 6, 12, 16, 20, 24); 
+
             // List<Integer> workersList = List.of(1);
             // ========================================================================
             // Scenario with high workers:
